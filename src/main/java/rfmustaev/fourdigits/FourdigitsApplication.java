2c43c1eacb1054af823bcc1b86263acd2d930656
Roman Mustaev <rfmustaev@gmail.com>
2020-01-29 12:23:40 +0400

    Initial commit

---
+++ src/main/java/rfmustaev/fourdigits/FourdigitsApplication.java
@@ -0,0 +1,11 @@
+package rfmustaev.fourdigits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourdigitsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FourdigitsApplication.class, args);
+	}
+}
---
+++ src/test/java/rfmustaev/fourdigits/FourdigitsApplicationTests.java
@@ -0,0 +1,13 @@
+package rfmustaev.fourdigits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourdigitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
